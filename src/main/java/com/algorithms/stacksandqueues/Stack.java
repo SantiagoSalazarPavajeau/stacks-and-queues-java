e8d2f6bcf1b4239f6ba64ae79735bae414a4cffc
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-05-06 15:23:39 -0400

    add stack and tests

--- src/main/java/com/algorithms/stacksandqueues/Stack.java
+++ src/main/java/com/algorithms/stacksandqueues/Stack.java
@@ -2,5 +2,51 @@ package com.algorithms.stacksandqueues;
 
 public class Stack {
     //comment here for testing"
-    public int value = 1;
+//    public int value = 1;
+    class Node{
+        Object value;
+        Node next;
+        Node(Object value, Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node top;
+    private int size;
+
+    public Object peek(){
+        Object value = this.top.value;
+        return value;
+    }
+
+    public void push(Object element){
+        Node node = new Node(element, null);
+        if(this.top == null){
+            this.top = node;
+        }else{
+            Node tail = this.top;
+            this.top = node;
+            this.top.next = tail;
+        }
+        size++;
+    }
+
+    public boolean isEmpty(){
+        return this.top == null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Object pop(){
+        Object popped = this.top.value;
+        this.top = this.top.next;
+        size--;
+        return popped;
+    }
+
+
+
 }
--- src/test/java/com/algorithms/stacksandqueues/StacksAndQueuesApplicationTests.java
+++ src/test/java/com/algorithms/stacksandqueues/StacksAndQueuesApplicationTests.java
@@ -2,6 +2,11 @@ package com.algorithms.stacksandqueues;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.util.AssertionErrors.*;
+
+
+import java.util.Queue;
+
 
 @SpringBootTest
 class StacksAndQueuesApplicationTests {
@@ -11,8 +16,54 @@ class StacksAndQueuesApplicationTests {
 	}
 
 	@Test
-	public void queue_IsEmpty(){
-		
+	public void stack_IsEmpty(){
+		//Setup
+
+		//exec
+		Stack stack = new Stack();
+		// Assert
+		assertTrue("stack is empty", stack.isEmpty() );
+	}
+
+	@Test
+	public void stack_push_AddsNewObjectToTop(){
+		//Setup
+		Stack stack = new Stack();
+		int expectedSize = 2;
+		//Exec
+
+		stack.push(1);
+		stack.push(2);
+
+		assertEquals("is size 2 after pushing two items", expectedSize, stack.getSize());
+	}
+
+	@Test
+	public  void stack_peek_ReturnsTheTopObjectButLeavesIt(){
+		//Setup
+		Stack stack = new Stack();
+		String top = "top";
+
+		// exec
+		stack.push(top);
+
+		assertEquals("Returns top element without erasing it", top, stack.peek());
+	}
+
+	@Test
+	public void stack_pop_RemovesTopItemAndReturnsIt(){
+		Stack stack= new Stack();
+		String expected = "second";
+		int expectedSize = 1;
+
+
+		stack.push("first");
+		stack.push("second");
+
+
+		assertEquals("return second item we added", expected, stack.pop());
+		assertEquals("is size 1", expectedSize, stack.getSize());
+
 	}
 
 }
