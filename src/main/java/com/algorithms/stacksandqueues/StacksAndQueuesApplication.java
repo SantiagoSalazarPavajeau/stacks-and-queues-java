e0f072de64ae06e7e64793608588dbc9220e0eca
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-05-06 12:16:44 -0400

    Initial commit

---
+++ src/main/java/com/algorithms/stacksandqueues/StacksAndQueuesApplication.java
@@ -0,0 +1,13 @@
+package com.algorithms.stacksandqueues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StacksAndQueuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StacksAndQueuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorithms/stacksandqueues/StacksAndQueuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorithms.stacksandqueues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StacksAndQueuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
